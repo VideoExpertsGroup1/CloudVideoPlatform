c9f6f9e8d46e1cd24526326c878a9ff71e3c0f2a
Video Experts Group1 <VideoExpertsGroup1@users.noreply.github.com>
2016-09-29 19:52:38 +0700

    Update install.sh

--- CameraManager/Python/rpi.local_camera/install.sh
+++ CameraManager/Python/rpi.local_camera/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install --fix-missing python-pip libpython-dev libxslt1-dev libyaml-dev gstreamer1.0-tools
-sudo python -m pip install -r requirements.txt <-- requirements.txt file should contain all python script deps
+sudo apt-get install --fix-missing python-pip libpython-dev libxslt1-dev libyaml-dev
+sudo python -m pip install -r requirements.txt 
 chmod -R 777 . 
 sudo su -c 'echo 'bcm2835_v4l2' >> /etc/modules' 
 sudo cp config.txt /boot/config.txt
