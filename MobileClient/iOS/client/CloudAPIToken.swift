49e892f061bd7350d15b22762963602d9f801b69
Video Experts Group1 <VideoExpertsGroup1@users.noreply.github.com>
2016-10-24 11:59:40 +0700

    Update CloudAPIToken.swift

--- MobileClient/iOS/client/CloudAPIToken.swift
+++ MobileClient/iOS/client/CloudAPIToken.swift
@@ -1,9 +1,18 @@
 //
 //  CloudAPIToken.swift
-//  VXGCloudClient
-//
+//  VXG Cloud Client
 //  Created by sea-kg on 16.06.16.
-//  Copyright © 2016 Video Experts Group. All rights reserved.
+//
+//  Copyright © 2016 EVE Group Pte. Ltd. All rights reserved.
+//  Contact: https://www.videoexpertsgroup.com/contact-vxg/
+//  This file is part of the demonstration of the VXG Cloud Platform.
+//
+//  Commercial License Usage
+//  Licensees holding valid commercial VXG licenses may use this file in
+//  accordance with the commercial license agreement provided with the
+//  Software or, alternatively, in accordance with the terms contained in
+//  a written agreement between you and EVE Group. For further information
+//  use the contact form at https://www.videoexpertsgroup.com/contact-vxg/
 //
 
 import Foundation
