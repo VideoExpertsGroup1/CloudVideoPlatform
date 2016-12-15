5c6e3f17ec8303d986ad09161841ec377d341112
Anatoly Zakharov <anatoly@videoexpertsgroup.com>
2016-12-15 11:54:57 +0700

    Make it more user-friendly

--- CameraManager/Python/rpi.local_camera/README.md
+++ CameraManager/Python/rpi.local_camera/README.md
@@ -9,39 +9,38 @@
 
 2. Besides python framework you'll need to install additional python libraries using this command (as admin\root user):
 
-    sudo apt-get install --fix-missing python-pip libpython-dev libxslt1-dev libyaml-dev
-    
-    sudo python -m pip install -r <PATH_TO_CM_DIR>/requirements.txt
+        sudo apt-get install --fix-missing python-pip libpython-dev libxslt1-dev libyaml-dev
+        sudo python -m pip install -r <PATH_TO_CM_DIR>/requirements.txt
 
 3. Copy all files to a local folder on rPI and set the files execute permission.
   
-    sudo chmod -R 777 *
+        sudo chmod -R 777 *
     
 4.   Add video driver loading on device start . Please note that reboot is required after following command:
 
-    sudo su -c 'echo 'bcm2835_v4l2' >> /etc/modules'
-    
-    sudo echo gpu_mem=128 >> /boot/config.txt
-    
-    sudo reboot;
+        sudo su -c 'echo 'bcm2835_v4l2' >> /etc/modules'
+        sudo echo gpu_mem=128 >> /boot/config.txt
+        sudo reboot;
     
     
 
 ## LAUNCH
-* Go to 
+1. Configure your video source by the one of following ways:
+    * Integration kit users should edit the file auth2\_cm\_starter.py and fill the variable REG_TOKEN with obtained Reg Token like this:
 
-    cnvrclient2.videoexpertsgroup.com/accounts/login/ 
+            REG_TOKEN = 'ABCDEF123456'
 
-  and Sign Up, if you already have an account on this site, skip this step.
+    * Other users should: 
+        * [Sign Up](http://cnvrclient2.videoexpertsgroup.com/), if you already have an account, skip this step;
+        * Open auth2\_cm\_starter.py and edit 'username' and 'password' fields of ACCP_CREDENTIALS structure with your credentials like this:
 
-* Open auth2_cm_starter.py and edit 'username' and 'password' variables with your credentials like this:
+                ACCP_CREDENTIALS = {
+                    'username': 'my_acc',
+                    'password': 'my_secure_pass',
+                }
 
-    username = 'my_acc'    
-    password = 'my_secure_pass'
+2. Launch application:
+
+        python auth2_cm_starter.py
     
-* Launch application:
-
-    python auth2_cm_starter.py
-    
-* After some time (camera registration takes 1-2 minutes), go to cnvrclient2.videoexpertsgroup.com/accounts/login/ 
-  and login with your credentials, click to 'Start video streaming' and you'll see the camera list with your Raspberry PI.
+3. After some time (camera registration takes 1-2 minutes), [login](http://cnvrclient2.videoexpertsgroup.com/) with your credentials, click to 'Start video streaming' and you'll see the camera list with your Raspberry PI.
--- CameraManager/Python/rpi.local_camera/auth2_cm_starter.py
+++ CameraManager/Python/rpi.local_camera/auth2_cm_starter.py
@@ -11,48 +11,63 @@ import globals
 import auth2_webapi_wrapper
 
 """
-Edit these fields
+Configuration section
 """
-# AccP server address without trailing slash
-accp_host = 'http://cnvrclient2.videoexpertsgroup.com'
-# AccP user credentials to register CM to
-username = ''
-password = ''
-# Source camera feed to retranslate to server. Supported following feeds:
+# If you already obtained the reg_token, please insert it here..
+REG_TOKEN = ''
+# ..If you don't, enter your AccP user credentials to register CM to
+ACCP_CREDENTIALS = {
+    'username': '',
+    'password': '',
+}
+
+# Source feed to retranslate to server. The following types of feeds are supported:
 #  - RTSP : 'rtsp://sample.url/feed_address'
 #  - hardware video source: 'dev:///dev/video0' for *nix /dev/video0 device
 #  - local file: 'file://c/dir/file.mp4' for WIN and 'file:///dir/file.mp4' for *nix
-src_camera_feed = 'dev:///dev/video0'
-# CM server address. Do not edit that if you not sure what does it means.
-cm_host = 'cam.skyvr.videoexpertsgroup.com'
+SRC_FEED = 'dev:///dev/video0'
+
+# Advanced configuration. Normally you should not edit these values.
+HOSTS = {
+    # AccP server address without trailing slash
+    'accp': 'http://cnvrclient2.videoexpertsgroup.com',
+    # CM server address
+    'cm': 'cam.skyvr.videoexpertsgroup.com',
+}
+"""
+End of configuration section
+"""
 
 globals.init_config()
 
 cm_registration_required = not globals.config.get('connection_id', '') or \
-                           globals.config.get('server_hostname', '') != cm_host
+                           globals.config.get('server_hostname', '') != HOSTS['cm']
 cm_registered = False
 if cm_registration_required:
-    api = auth2_webapi_wrapper.Auth2WebAPIWrapper(accp_host)
-    try:
-        api.login(username, password)
-        token = api.create_regtoken()['token']
-
-        globals.config['reg_token'] = token
-        globals.config['camera_feed'] = src_camera_feed
-        globals.config['server_hostname'] = cm_host
-        globals.config['connection_id'] = ""
-        globals.config['camera_uuid'] = ""
-        globals.config.write()
-
-        cm_registered = True
-    except Exception as e:
-        print e
-
-    if api.is_connected():
+    if REG_TOKEN:
+        globals.config['reg_token'] = REG_TOKEN
+    elif ACCP_CREDENTIALS['username']:
+        api = auth2_webapi_wrapper.Auth2WebAPIWrapper(HOSTS['accp'])
         try:
-            api.logout()
-        except:
-            pass
+            api.login(**ACCP_CREDENTIALS)
+            globals.config['reg_token'] = api.create_regtoken()['token']
+        except Exception as e:
+            raise RuntimeError('Failed to obtain reg_token from AccP: %s' % e)
+        finally:
+            if api.is_connected():
+                try:
+                    api.logout()
+                except:
+                    pass
+    else:
+        raise RuntimeError('Configuration error: Please specify either REG_TOKEN or ACCP_CREDENTIALS')
+
+    globals.config['camera_feed'] = SRC_FEED
+    globals.config['server_hostname'] = HOSTS['cm']
+    globals.config['connection_id'] = ""
+    globals.config['camera_uuid'] = ""
+    globals.config.write()
+    cm_registered = True
 
 if not cm_registration_required or (cm_registration_required and cm_registered):
     client.init_signals()
