62c547de4d63f47fcd71cbbb63654dc3ba9043b4
Anatoly Zakharov <anatoly@videoexpertsgroup.com>
2017-02-14 15:44:03 +0700

    Removed direct AccP registration support; Added RTMP source help string

--- CameraManager/Python/rpi.local_camera/auth2_cm_starter.py
+++ CameraManager/Python/rpi.local_camera/auth2_cm_starter.py
@@ -8,29 +8,22 @@ When config is absent, or last time CM was connected to another server, script r
 
 import client
 import globals
-import auth2_webapi_wrapper
 
 """
 Configuration section
 """
-# If you already obtained the reg_token, please insert it here..
+# Please insert the reg_token here
 REG_TOKEN = ''
-# ..If you don't, enter your AccP user credentials to register CM to
-ACCP_CREDENTIALS = {
-    'username': '',
-    'password': '',
-}
 
 # Source feed to retranslate to server. The following types of feeds are supported:
 #  - RTSP : 'rtsp://sample.url/feed_address'
 #  - hardware video source: 'dev:///dev/video0' for *nix /dev/video0 device
 #  - local file: 'file://c/dir/file.mp4' for WIN and 'file:///dir/file.mp4' for *nix
+#  - RTMP live streams:  'rtmp://sample.url/app/session'
 SRC_FEED = 'dev:///dev/video0'
 
 # Advanced configuration. Normally you should not edit these values.
 HOSTS = {
-    # AccP server address without trailing slash
-    'accp': 'http://cnvrclient2.videoexpertsgroup.com',
     # CM server address
     'cm': 'cam.skyvr.videoexpertsgroup.com',
 }
@@ -46,21 +39,8 @@ cm_registered = False
 if cm_registration_required:
     if REG_TOKEN:
         globals.config['reg_token'] = REG_TOKEN
-    elif ACCP_CREDENTIALS['username']:
-        api = auth2_webapi_wrapper.Auth2WebAPIWrapper(HOSTS['accp'])
-        try:
-            api.login(**ACCP_CREDENTIALS)
-            globals.config['reg_token'] = api.create_regtoken()['token']
-        except Exception as e:
-            raise RuntimeError('Failed to obtain reg_token from AccP: %s' % e)
-        finally:
-            if api.is_connected():
-                try:
-                    api.logout()
-                except:
-                    pass
     else:
-        raise RuntimeError('Configuration error: Please specify either REG_TOKEN or ACCP_CREDENTIALS')
+        raise RuntimeError('Configuration error: Please specify REG_TOKEN')
 
     globals.config['camera_feed'] = SRC_FEED
     globals.config['server_hostname'] = HOSTS['cm']
