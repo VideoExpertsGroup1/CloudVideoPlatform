fb9b45387056b7c14a1ec27553b909bb7447360f
Anatoly Zakharov <anatoly@videoexpertsgroup.com>
2017-06-20 11:52:48 +0700

    Added executable permissions to needed files, removed chmod from install.sh

--- CameraManager/Python/rpi.local_camera/install.sh
+++ CameraManager/Python/rpi.local_camera/install.sh
@@ -3,7 +3,6 @@
 sudo apt-get update
 sudo apt-get install --fix-missing python-pip libpython-dev libxslt1-dev libyaml-dev
 sudo python -m pip install -r requirements.txt 
-chmod -R 777 . 
 sudo su -c 'echo 'bcm2835_v4l2' >> /etc/modules' 
 sudo cp config.txt /boot/config.txt
 sudo reboot
